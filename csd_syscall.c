d28b99090b486ac9fab3b12d1819fc216ca7f7df
S M Riad Hossain <96090060+SMRiadHossain@users.noreply.github.com>
2023-02-10 14:32:58 +0600

    Add files via upload

--- csd_syscall.c
+++ csd_syscall.c
@@ -1,8 +1,9 @@
-/* this one is based on syscall and struct parameter passing as you first told to implement syscall for device interface with struct. */
-
 #include<linux/kernel.h>
 #include<linux/syscalls.h>
 #include<linux/uaccess.h>
+#include <linux/nvme_ioctl.h>
+#include <linux/nvme.h>
+#include <linux/blkdev.h>
 
 #define INITIALIZE_BUCKET	'a'
 #define GET_BUCKET_INFO		'b'
@@ -17,7 +18,7 @@ struct buff{
 struct csd_params {
 
 	int ObjectID;
-	char *data_pointer;
+	char __user *data_pointer;
 	struct buff buff_params;
 };
 
@@ -37,47 +38,119 @@ void osl_externalinterface_getbucketinfo(struct csd_params *k_para)
 
 void osl_externalinterface_putobject(struct csd_params *k_para)
 {
+
 	printk("osl_externalinterface_putobject function executed\n");
+	
+	struct nvme_user_io uio;
+	struct file *fd = filp_open("/dev/nvme0n1", O_WRONLY | O_DIRECT, 0);
+	
+	struct inode* inode = (fd->f_mapping->host);
+	struct block_device* bdev = I_BDEV(inode);
+	struct gendisk* bd_disk = (struct gendisk*)bdev->bd_disk;
+	
+	//Here we will populate nvme_user_io
+
+	uio.opcode = nvme_cmd_write;
+	uio.flags = 0;
+	uio.control = 0;
+	uio.metadata = (unsigned long) 0;
+	uio.addr = (unsigned long) k_para->data_pointer; // data;
+	uio.slba = 0;
+	uio.nblocks = 1;struct file *fd = filp_open("/dev/nvme0n1", O_WRONLY, 0);
+	
+	struct inode* inode = (fd->f_mapping->host);
+	struct block_device* bdev = I_BDEV(inode);
+	uio.dsmgmt = 0;
+	uio.reftag = 0;
+	uio.apptag = 0;
+	uio.appmask = 0;
+
+	//mm_segment_t old_fs = get_fs();
+	//set_fs(KERNEL_DS);
+	mm_segment_t old_fs;
+	ssize_t result;
+
+	old_fs = get_fs();
+	set_fs(get_ds());
+
+
+	// fd->f_op->unlocked_ioctl(fd, NVME_IOCTL_SUBMIT_IO, (unsigned long) &uio);
+	bd_disk->fops->ioctl(bdev, 0, NVME_IOCTL_SUBMIT_IO, (unsigned long) &uio);
+	set_fs(old_fs);
+
+	filp_close(fd, 0);
+
 	return;
 }
 
 void osl_externalinterface_getobject(struct csd_params *k_para)
 {
 	printk("osl_externalinterface_getobject function executed\n");
+	
+	struct nvme_user_io uio;
+	struct file *fd = filp_open("/dev/nvme0n1", O_WRONLY, 0);
+	
+	struct inode* inode = (fd->f_mapping->host);
+	struct block_device* bdev = I_BDEV(inode);
+	struct gendisk* bd_disk = (struct gendisk*)bdev->bd_disk;
+	
+	//Here we will populate nvme_user_io
+
+	uio.opcode = nvme_cmd_read;
+	uio.flags = 0;
+	uio.control = 0;
+	uio.metadata = (unsigned long) 0;
+	uio.addr = (void __user *) k_para->data_pointer; // data;
+	uio.slba = 0;
+	uio.nblocks = 1;
+	uio.dsmgmt = 0;
+	uio.reftag = 0;
+	uio.apptag = 0;
+	uio.appmask = 0;
+
+	//mm_segment_t old_fs = get_fs();
+	//set_fs(KERNEL_DS);
+	mm_segment_t old_fs;
+	ssize_t result;
+
+	old_fs = get_fs();
+	set_fs(get_ds());
+
+
+	// fd->f_op->unlocked_ioctl(fd, NVME_IOCTL_SUBMIT_IO, (unsigned long) &uio);
+	bd_disk->fops->ioctl(bdev, 0, NVME_IOCTL_SUBMIT_IO, (void __user *) &uio);
+	set_fs(old_fs);
+
+	filp_close(fd, 0);
+
 	return;
 }
 
 void osl_externalinterface_delobject(struct csd_params *k_para)
 {
 	printk("osl_externalinterface_delobject function execute\n");
+
+	struct file *fd = filp_open("/dev/nvme0n1", O_WRONLY, 0);
+	struct inode* inode = (fd->f_mapping->host);
+	struct block_device* bdev = I_BDEV(inode);
+
+	blkdev_ioctl(bdev, 0, BLKDISCARD, k_para->data_pointer, 4096);
+
 	return;
 }
 
 
-
-
-//336
-SYSCALL_DEFINE1(csd_syscall, const void*, user_para) {
+SYSCALL_DEFINE1(csd_syscall, const void __user *, user_para) {
 
 	struct csd_params k_para;
 
 	copy_from_user(&k_para,(struct csd_params*) user_para,sizeof(struct csd_params));//copy from user function uaccess.h
 
 	//struct buff buff1;
-    printk("I'm here\n");
 
 	char CommandID = k_para.buff_params.command[0];
-	char BucketID = k_para.buff_params.command[1];
 
-    printk("%c %c\n", CommandID, BucketID);
-
-	/*if(!strncpy_from_user(k_bucketID, u_bucketID, 255)) {//have to improve
-	  printk("Error resolving parameters\n");
-	  return -1;  
-	  }*/
-
-	/*if(copy_from_user(&data, DPTR, c_ID)
-	  printk("Bucket-> %s created.\n", k_bucketID);*/
+	printk("%c\n", CommandID);
 
 	switch(CommandID)
 	{
@@ -101,5 +174,3 @@ SYSCALL_DEFINE1(csd_syscall, const void*, user_para) {
 	}
 	return 0;
 }
-
-MODULE_LICENSE("GPL");
--- test.c
+++ test.c
@@ -3,13 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-
 #define MAX_KV_KEY_SIZE		16
-#define __NR_csd_syscall		294
-
-
-
+#define __NR_csd_syscall	294
 
 struct buff{
 	char command[4];
@@ -18,24 +13,44 @@ struct buff{
 struct csd_params {
 
 	int ObjectID;
-	char *data_pointer;
+	char* data_pointer;
 	struct buff buffer1;
 } user_para;
 
 
 
-int main(/*int argc, char **argv*/)
+int main(int argc, char **argv)
 {
-	user_para.buffer1.command[0]= 'a';
-	user_para.buffer1.command[1]='0';
-	user_para.ObjectID= 0;
+	char w_data[4096] = "ABCDEabcde";
+	char r_data[4096];
 
+	user_para.buffer1.command[0]= 'c';
+	user_para.data_pointer = w_data;
+	
+	if(!syscall(__NR_csd_syscall,(void*) &user_para)) {
+		printf("Systemcall successfull\n");
+	} else {
+		printf("Error systemcall\n");
+	}
+	
+	user_para.buffer1.command[0]= 'd';
+	user_para.data_pointer = r_data;
+	
 	if(!syscall(__NR_csd_syscall,(void*) &user_para)) {
 		printf("Systemcall successfull\n");
 	} else {
 		printf("Error systemcall\n");
 	}
 
+	printf("r_data: %s\n", r_data);
+	
+	user_para.buffer1.command[0]= 'e';
+	
+	if(!syscall(__NR_csd_syscall,(void*) &user_para)) {
+		printf("Systemcall successfull\n");
+	} else {
+		printf("Error systemcall\n");
+	}
 
 	return 0;
 }
